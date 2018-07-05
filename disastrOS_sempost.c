5ac198b8a27659ec17192ef055f858cc37894c1f
FerraroJacopo <39092845+FerraroJacopo@users.noreply.github.com>
2018-07-05 13:24:01 +0200

    Update disastrOS_sempost.c
    implement internal_semPost

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -7,5 +7,29 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semPost(){
-  // do stuff :)
+  
+  int fd = running->syscall_args[0]; //The argument of the semaphore is file descriptor
+  
+  SemDescriptor* desc = SemDescriptorList_byFd(&running->sem_descriptors, fd); //Get the semaphore descriptor from fd
+
+  printf("Pid %d requesting sempost on %d semaphore\n", running->pid, desc->semaphore->id);
+
+
+  Semaphore* sem = desc->semaphore;
+  sem->count++;
+  
+  SemDescriptorPtr* desc_ptr;
+  
+  if (sem->count <= 0) {
+        
+        List_insert(&ready_list, ready_list.last, (ListItem*) running);
+        desc_ptr = (SemDescriptorPtr*) List_detach(&sem->waiting_descriptors, (ListItem*) sem->waiting_descriptors.first);
+        List_insert(&sem->descriptors, sem->descriptors.last, (ListItem*) desc_ptr);
+        List_detach(&waiting_list, (ListItem*) desc_ptr->descriptor->pcb);
+        running->status = Ready;  // Set ready process state
+        running = desc_ptr->descriptor->pcb;  
+    }
+
+    running->syscall_retvalue = 0;
+    return;
 }
