5ac6a38d0d9342f13123bc1c584af9a7c5a68e2b
D'Amato Fabio <38558878+AbruzzoDAmatoFerraro@users.noreply.github.com>
2018-07-05 16:04:23 +0200

    Update internal_semopen.c

--- internal_semopen.c
+++ internal_semopen.c
@@ -5,7 +5,46 @@
 #include "disastrOS_syscalls.h"
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
+#include "disastrOS_globals.h"
+#include "disastrOS_constants.h"
 
 void internal_semOpen(){
-  // do stuff :)
+
+    int id = running->syscall_args[0];
+    int count = running->syscall_args[1];
+    
+    Semaphore* sem = SemaphoreList_byId(semaphores_list, id);
+
+    if (count < 0){
+        running->syscall_retvalue = DSOS_ESEMAPHOREOPEN;
+        return;
+    }
+    
+    if (!sem) {
+        sem=Semaphore_alloc(id, count);
+        List_insert(&semaphores_list, semaphores_list.last, (ListItem*) sem);
+    }
+    
+    int fd = running->last_sem_fd; 
+    SemDescriptor* des = SemDescriptor_alloc(fd, sem, running);
+    
+    if (!des) {
+        running->syscall_retvalue = DSOS_ESEMDESCALLOC;
+        return;
+    }
+    
+    running->last_sem_fd++;
+    List_insert(&running->sem_descriptors, running->sem_descriptors.last, (ListItem*) des);
+    
+    SemDescriptorPtr* ptr_desc = SemDescriptorPtr_alloc(des);
+    if (ptr_desc) {
+        running->syscall_retvalue = DSOS_ESEMAPHOREDESC;
+        return;
+    }
+    
+    des->ptr=ptr_desc;
+    List_insert(&sem->descriptors, sem->descriptors.last, (ListItem*) ptr_desc);
+
+    running->syscall_retvalue = des->fd;
+    return;
 }
