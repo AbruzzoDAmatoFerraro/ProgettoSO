34ea987f46e918c4106bbd131b0ed263965a5623
FerraroJacopo <39092845+FerraroJacopo@users.noreply.github.com>
2018-07-12 13:14:03 +0200

    add sem_close

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -7,5 +7,33 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semClose(){
-  // do stuff :)
+
+    int fd = running->syscall_args[0];
+    SemDescriptor* sem_desc = SemDescriptorList_byFd(&running->sem_descriptors, fd);
+
+    if(!sem_desc){
+        running->syscall_retvalue = DSOS_ESEMAPHORENOFD;
+        return;
+    }
+
+    List_detach(&running->sem_descriptors, (ListItem*)sem_desc);
+    Semaphore* sem = sem_desc->semaphore;
+    
+    if(!sem){
+        running->return_value=DSOS_ESEMAPHORENOAVAILABLE;
+        return;
+    }
+
+    SemDescriptorPtr* sem_desc_ptr = (SemDescriptorPtr*)List_detach(&(sem->descriptors), (ListItem*)(sem_desc->ptr));
+    assert(sem_desc_ptr);
+
+    if(sem->descriptors.size == 0 && sem->waiting_descriptors.size==0){
+        List_detach(&semaphores_list, (ListItem*)sem);
+        Semaphore_free(sem);
+    }
+
+    SemDescriptor_free(sem_desc);
+    SemDescriptorPtr_free(sem_desc_ptr);
+    running->syscall_retvalue = 0;
+    return;
 }
