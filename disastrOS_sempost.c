47d62a6cda5a519e3a53a2860ed43b144dac1eb2
GiulianoAbruzzo <39120917+GiulianoAbruzzo@users.noreply.github.com>
2018-07-12 13:25:18 +0200

    #Fix sempost

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -8,50 +8,32 @@
 
 void internal_semPost(){
   
-  int fd = running->syscall_args[0]; //The argument of the semaphore is file descriptor
+    int fd = running->syscall_args[0]; 
+    SemDescriptor* desc = SemDescriptorList_byFd(&running->sem_descriptors, fd); 
   
-  SemDescriptor* desc = SemDescriptorList_byFd(&running->sem_descriptors, fd); //Get the semaphore descriptor from fd
+    if(!desc){
+        running->syscall_retvalue=DSOS_ESEMAPHORENOFD;
+		return;
+    }
+
+    Semaphore* sem = desc->semaphore;
   
-  if(!desc){
-		running->syscall_retvalue=DSOS_ESEMAPHORENOFD;
+    if(!sem){
+		running->syscall_retvalue= DSOS_ESEMAPHORENOAVAILABLE;
 		return;
 	}
 
-  printf("Pid %d requesting sempost on %d semaphore\n", running->pid, desc->semaphore->id);
-
-
-  Semaphore* sem = desc->semaphore;
+    sem->count++;
+    SemDescriptorPtr* sem_des_ptr;
   
-  if(!sem){
-		running->syscall_retvalue= DSOS_ESEMAPHORENOAVAILABLE;
-		return;
-	 }
-
-  
-  sem->count++;
-  
-  SemDescriptorPtr* sem_des_ptr;
-  
-  if (sem->count <= 0) {
-        
-        sem_des_ptr = (SemDescriptorPtr*)List_detach(&sem->waiting_descriptors, sem->waiting_descriptors.first);
-        
-        if(!sem_des_ptr){
-            running->syscall_retvalue = DSOS_ERESOURCEOPEN;
-            return; }
-    
-        List_detach(&waiting_list, (ListItem*)sem_des_ptr->descriptor->pcb); //pcb removed from waiting list
-
-        List_insert(&ready_list, ready_list.last, (ListItem*)sem_des_ptr->descriptor->pcb); //add process to the ready list
-        
-	      List_insert(&sem->descriptors, sem->descriptors.last, (ListItem*) sem_des_ptr);
-        
-
-        sem_des_ptr->descriptor->pcb->status = Ready;
-
-
+    if (sem->count <= 0) {
+        List_insert(&ready_list, ready_list.last, (ListItem*) running);
+        sem_des_ptr=(SemDescriptorPtr*) List_detach(&sem->waiting_descriptors, (ListItem*) sem->waiting_descriptors.first);
+        List_insert(&sem->descriptors, sem->descriptors.last, (ListItem*) sem_des_ptr);
+        List_detach(&waiting_list, (ListItem*) sem_des_ptr->descriptor->pcb);
+        running->status = Ready;
+        running = sem_des_ptr->descriptor->pcb;
     }
     running->syscall_retvalue = 0;
-   
 	return;
 }
